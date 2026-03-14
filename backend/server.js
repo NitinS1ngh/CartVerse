18dda59833fb363531b3e7fd95e0a0ec848fa9da
Nitin Singh <ns23241802@gmail.com>
2026-03-15 00:49:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ import orderRouter from './routes/orderRoute.js';
 // App config
 
 const app = express()
-const port = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 connectDB()
 connectCloudinary()
 
